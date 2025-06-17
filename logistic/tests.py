9a509ce8f60538a4d78977ec1797c151803a3f44
Alan Le <aal.py@yandex.ru>
2025-06-17 17:07:34 +0400

    Add pipline

---
+++ logistic/tests.py
@@ -0,0 +1,8 @@
+from rest_framework.test import APIClient
+from unittest import TestCase
+
+class Testxxx(TestCase):
+    def test_sample_view(self):
+        client = APIClient()
+        response = client.get('/api/v1/test')
+        self.assertEqual(response.status_code,200)
